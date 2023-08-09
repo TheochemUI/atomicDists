442c3689ed2d5f3007332dc53c100e312593b92a
Rohit Goswami <rog32@hi.is>
2023-08-09 11:51:13 +0000

    ENH: Add concatenation functionality

--- CppCore/gtests/TestSimpleMatrix.cc
+++ CppCore/gtests/TestSimpleMatrix.cc
@@ -63,3 +63,47 @@ TEST(SimpleMatrixTest, ConstColumnAccessor) {
     auto col = mat.column(1);
     EXPECT_TRUE(std::equal(col.begin(), col.end(), std::vector<int>{2, 5}.begin()));
 }
+
+TEST(SimpleMatrixConcatenationTest, HorizontalConcatenation) {
+    SimpleMatrix<int> mat1{{1, 2}, {3, 4}};
+    SimpleMatrix<int> mat2{{5, 6}, {7, 8}};
+    SimpleMatrix<int> result = concatenate_horizontal(mat1, mat2);
+    EXPECT_EQ(result.rows(), 2);
+    EXPECT_EQ(result.cols(), 4);
+    EXPECT_EQ(result(0, 0), 1);
+    EXPECT_EQ(result(0, 1), 2);
+    EXPECT_EQ(result(0, 2), 5);
+    EXPECT_EQ(result(0, 3), 6);
+    EXPECT_EQ(result(1, 0), 3);
+    EXPECT_EQ(result(1, 1), 4);
+    EXPECT_EQ(result(1, 2), 7);
+    EXPECT_EQ(result(1, 3), 8);
+}
+
+TEST(SimpleMatrixConcatenationTest, VerticalConcatenation) {
+    SimpleMatrix<int> mat1{{1, 2}, {3, 4}};
+    SimpleMatrix<int> mat2{{5, 6}, {7, 8}};
+    SimpleMatrix<int> result = concatenate_vertical(mat1, mat2);
+    EXPECT_EQ(result.rows(), 4);
+    EXPECT_EQ(result.cols(), 2);
+    EXPECT_EQ(result(0, 0), 1);
+    EXPECT_EQ(result(0, 1), 2);
+    EXPECT_EQ(result(1, 0), 3);
+    EXPECT_EQ(result(1, 1), 4);
+    EXPECT_EQ(result(2, 0), 5);
+    EXPECT_EQ(result(2, 1), 6);
+    EXPECT_EQ(result(3, 0), 7);
+    EXPECT_EQ(result(3, 1), 8);
+}
+
+TEST(SimpleMatrixConcatenationTest, MismatchedRowsForHorizontalConcatenation) {
+    SimpleMatrix<int> mat1(2, 2);
+    SimpleMatrix<int> mat2(3, 2);
+    EXPECT_THROW(concatenate_horizontal(mat1, mat2), std::invalid_argument);
+}
+
+TEST(SimpleMatrixConcatenationTest, MismatchedColsForVerticalConcatenation) {
+    SimpleMatrix<int> mat1(2, 2);
+    SimpleMatrix<int> mat2(2, 3);
+    EXPECT_THROW(concatenate_vertical(mat1, mat2), std::invalid_argument);
+}
--- CppCore/include/SimpleMatrix.hpp
+++ CppCore/include/SimpleMatrix.hpp
@@ -1,50 +1,99 @@
 #pragma once
 
 #include <cstddef>
-#include <vector>
 #include <iostream>
 #include <range/v3/view/iota.hpp>
 #include <range/v3/view/transform.hpp>
+#include <range/v3/algorithm/copy.hpp>
+#include <vector>
 
 namespace atmdist::types {
+template <typename T> class SimpleMatrix;
+template <typename T> SimpleMatrix<T> concatenate_horizontal(const SimpleMatrix<T>& mat1, const SimpleMatrix<T>& mat2);
+template <typename T> SimpleMatrix<T> concatenate_vertical(const SimpleMatrix<T>& mat1, const SimpleMatrix<T>& mat2);
 
-template <typename T>
-class SimpleMatrix {
+
+template <typename T> class SimpleMatrix {
 public:
-    SimpleMatrix(size_t rows, size_t cols) : data(rows, std::vector<T>(cols, 0)) {}
-    SimpleMatrix(size_t rows, size_t cols, T value)
-        : data(rows, std::vector<T>(cols, value)) {}
-    SimpleMatrix(std::initializer_list<std::initializer_list<T>> values)
-        : data(values.begin(), values.end()) {}
+  SimpleMatrix(size_t rows, size_t cols)
+      : data(rows, std::vector<T>(cols, 0)) {}
+  SimpleMatrix(size_t rows, size_t cols, T value)
+      : data(rows, std::vector<T>(cols, value)) {}
+  SimpleMatrix(std::initializer_list<std::initializer_list<T>> values)
+      : data(values.begin(), values.end()) {}
 
-    T& operator()(size_t row, size_t col) { return data[row][col]; }
-    const T& operator()(size_t row, size_t col) const { return data[row][col]; }
+  T &operator()(size_t row, size_t col) { return data[row][col]; }
+  const T &operator()(size_t row, size_t col) const { return data[row][col]; }
 
-    size_t rows() const { return data.size(); }
-    size_t cols() const { return data.empty() ? 0 : data[0].size(); }
+  size_t rows() const { return data.size(); }
+  size_t cols() const { return data.empty() ? 0 : data[0].size(); }
 
-    friend std::ostream& operator<<(std::ostream& os, const SimpleMatrix<T>& mat) {
-        for (const auto& row : mat.data) {
-            for (const auto& val : row) {
-                os << val << ' ';
-            }
-            os << '\n';
-        }
-        return os;
+  friend std::ostream &operator<<(std::ostream &os,
+                                  const SimpleMatrix<T> &mat) {
+    for (const auto &row : mat.data) {
+      for (const auto &val : row) {
+        os << val << ' ';
+      }
+      os << '\n';
     }
-    std::vector<T>& row(size_t row) { return data[row]; }
-    const std::vector<T>& row(size_t row) const { return data[row]; }
-    auto column(size_t col) {
-        return ranges::views::iota(size_t(0), data.size())
-             | ranges::views::transform([this, col](size_t row) { return std::ref(data[row][col]); });
-    }
-    auto column(size_t col) const {
-        return ranges::views::iota(size_t(0), data.size())
-             | ranges::views::transform([this, col](size_t row) -> const T& { return data[row][col]; });
-    }
+    return os;
+  }
+  std::vector<T> &row(size_t row) { return data[row]; }
+  const std::vector<T> &row(size_t row) const { return data[row]; }
+  auto column(size_t col) {
+    return ranges::views::iota(size_t(0), data.size()) |
+           ranges::views::transform(
+               [this, col](size_t row) { return std::ref(data[row][col]); });
+  }
+  auto column(size_t col) const {
+    return ranges::views::iota(size_t(0), data.size()) |
+           ranges::views::transform(
+               [this, col](size_t row) -> const T & { return data[row][col]; });
+  }
+
+  friend SimpleMatrix
+  concatenate_horizontal<>(const SimpleMatrix &mat1,
+                                                   const SimpleMatrix &mat2);
+  friend SimpleMatrix
+  concatenate_vertical<>(const SimpleMatrix &mat1,
+                                                 const SimpleMatrix &mat2);
 
 private:
-    std::vector<std::vector<T>> data;
+  std::vector<std::vector<T>> data;
 };
+
+
+template <typename T>
+SimpleMatrix<T> concatenate_horizontal(const SimpleMatrix<T> &mat1,
+                                       const SimpleMatrix<T> &mat2) {
+  if (mat1.rows() != mat2.rows()) {
+    throw std::invalid_argument(
+        "Number of rows must be the same for horizontal concatenation");
+  }
+
+  SimpleMatrix<T> result(mat1.rows(), mat1.cols() + mat2.cols());
+  for (size_t i = 0; i < mat1.rows(); ++i) {
+    auto row1 = mat1.row(i);
+    auto row2 = mat2.row(i);
+    ranges::copy(row1, result.row(i).begin());
+    ranges::copy(row2, result.row(i).begin() + mat1.cols());
+  }
+  return result;
+}
+
+template <typename T>
+SimpleMatrix<T> concatenate_vertical(const SimpleMatrix<T> &mat1,
+                                     const SimpleMatrix<T> &mat2) {
+  if (mat1.cols() != mat2.cols()) {
+    throw std::invalid_argument(
+        "Number of columns must be the same for vertical concatenation");
+  }
+
+  SimpleMatrix<T> result(mat1.rows() + mat2.rows(), mat1.cols());
+  ranges::copy(mat1.data.begin(), mat1.data.end(), result.data.begin());
+  ranges::copy(mat2.data.begin(), mat2.data.end(),
+               result.data.begin() + mat1.rows());
+  return result;
+}
 
 } // namespace atmdist::types
